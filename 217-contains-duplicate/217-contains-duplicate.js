cfe0d0659af517d46c2650765c89b6e79eba522f
Jason Kuffler <84805346+Jkuffler@users.noreply.github.com>
2022-07-08 23:12:36 -0400

    Time: 115 ms (70.43%), Space: 50 MB (86.71%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    if(new Set(nums).size !== nums.length) {
+        return true;
+    } else return false;
+};
